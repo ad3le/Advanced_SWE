2900fa4958fc6b90f61db9ff0cf3637ba1a54334
Sonia Meehan <meehansonia@gmail.com>
2021-09-21 17:18:14 +0200

    tried to make cross origin requests possible

--- backend/src/main/java/com/example/demo/controllers/SentenceController.java
+++ backend/src/main/java/com/example/demo/controllers/SentenceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 class SentenceController {
 
@@ -16,16 +16,19 @@ class SentenceController {
         this.repository = repository;
     }
 
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @GetMapping("/sentences")
     List<Sentence> all() {
         return repository.findAll();
     }
 
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @PostMapping("/sentences")
     Sentence newSentence(@RequestBody Sentence newSentence) {
         return repository.save(newSentence);
     }
 
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @DeleteMapping("/sentences/{id}")
     void deleteSentence(@PathVariable String id) {
         repository.deleteById(id);
