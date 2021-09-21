1c62653acb9e87c5aeea813a1afaf629e520d583
Sonia Meehan <meehansonia@gmail.com>
2021-09-21 17:16:58 +0200

    made small changes to Sentence class to make it value object

--- backend/src/main/java/com/example/demo/models/Sentence.java
+++ backend/src/main/java/com/example/demo/models/Sentence.java
@@ -3,19 +3,33 @@ package com.example.demo.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Document(collection = "sentences")
-public class Sentence {
+public final class Sentence {
 
     @Id
     String id;
-    private String text;
-    private Language language;
+    private final String text;
+    private final Language language;
 
     public Sentence(String text, Language language) {
         this.text = text;
         this.language = language;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sentence sentence = (Sentence) o;
+        return text.equals(sentence.text) && language == sentence.language;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text, language);
+    }
 
     public String getId() {
         return id;
